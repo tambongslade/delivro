b32dad1d0ad514559544762caac3f11bffb747db
tambong <80495675+tambongslade@users.noreply.github.com>
2024-09-29 19:35:50 +0100

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -42,7 +42,7 @@ class _HomeState extends State<Home> {
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  "Top Categories",
+                  "Top Categories vivi",
                   style: HeadingStyle.copyWith(fontSize: 25),
                 ),
               ),
